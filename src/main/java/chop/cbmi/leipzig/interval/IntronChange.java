d4afdfc1a47c4a92f09dd9b9aea8efb6de143aa5
Jeremy Leipzig <leipzig@gmail.com>
2013-06-13 10:30:32 -0400

    trying to resolve intron probs such as _0

--- src/main/java/chop/cbmi/leipzig/interval/IntronChange.java
+++ src/main/java/chop/cbmi/leipzig/interval/IntronChange.java
@@ -27,7 +27,7 @@ public class IntronChange extends TranscriptChange {
 
         //intron
         //intronic nucleotides (coding DNA reference sequence only)
-        //beginning of the intron; the number of the last nucleotide of the preceding exon, a plus sign and the position in the intron, like c.77+1G, c.77+2T, ....
+        //beginning of the intron; the number of the last nucleotide of the preceeding exon, a plus sign and the position in the intron, like c.77+1G, c.77+2T, ....
         //end of the intron; the number of the first nucleotide of the following exon, a minus sign and the position upstream in the intron, like ..., c.78-2A, c.78-1G.
         //in the middle of the intron, numbering changes from "c.77+.." to "c.78-.."; for introns with an uneven number of nucleotides the central nucleotide is the last described with a "+"
         //for (Intron intron : introns)
@@ -38,14 +38,27 @@ public class IntronChange extends TranscriptChange {
                 int lastBefore = transcript.isStrandPlus() ? transcript.lastExonPositionBefore(seqChange.getStart()) : transcript.firstExonPositionAfter(seqChange.getStart());
                 int cdsFirstAfter= cdsBaseNumberOfExonInTx(firstAfter);
                 int cdsLastBefore= cdsBaseNumberOfExonInTx(lastBefore);
-                int distanceToPrecedingExonStart=Math.abs(seqChange.getStart()-lastBefore);
-                int distanceToProcedingExonStart=Math.abs(seqChange.getStart()-firstAfter);
-                int distanceToPrecedingExonEnd=Math.abs(seqChange.getEnd()-lastBefore);
-                int distanceToProcedingExonEnd=Math.abs(seqChange.getEnd()-firstAfter);
+                int distanceToPreceedingExonStart=Math.abs(seqChange.getStart()-lastBefore);
+                int distanceToProceedingExonStart=Math.abs(seqChange.getStart()-firstAfter);
+                int distanceToPreceedingExonEnd=Math.abs(seqChange.getEnd()-lastBefore);
+                int distanceToProceedingExonEnd=Math.abs(seqChange.getEnd()-firstAfter);
+
+                //665+1-665+2
+                int fromProceeding = (distanceToProceedingExonStart <= distanceToProceedingExonEnd) ? distanceToProceedingExonStart : distanceToProceedingExonEnd;
+                int toProceeding =  (distanceToProceedingExonStart <= distanceToProceedingExonEnd) ?  distanceToProceedingExonEnd : distanceToProceedingExonStart;
+
+                //666-3-666-2
+                int fromPreceeding = (distanceToPreceedingExonStart <= distanceToPreceedingExonEnd) ? distanceToPreceedingExonEnd : distanceToPreceedingExonStart;
+                int toPreceeding =  (distanceToPreceedingExonStart <= distanceToPreceedingExonEnd) ? distanceToPreceedingExonStart : distanceToPreceedingExonEnd;
+
+                String fromPreceedingString = ((fromPreceeding == 0) ? "" : "+"+String.valueOf(fromPreceeding));
+                String fromProceedingString = ((fromProceeding == 0) ? "" : "-"+String.valueOf(fromProceeding));
+                String toPreceedingString =   ((toPreceeding == 0)   ? "" : "+"+String.valueOf(toPreceeding));
+                String toProceedingString =   ((toProceeding == 0)   ? "" : "-"+String.valueOf(toProceeding));
 
                 if(seqChange.isDel()){
-                    String txPosStart = (distanceToPrecedingExonStart<distanceToProcedingExonStart) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPrecedingExonStart) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProcedingExonStart);
-                    String txPosEnd = (distanceToPrecedingExonEnd<distanceToProcedingExonEnd) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPrecedingExonEnd) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProcedingExonEnd);
+                    String txPosStart = (distanceToPreceedingExonStart<distanceToProceedingExonStart) ? String.valueOf(cdsLastBefore)+ fromPreceedingString : String.valueOf(cdsFirstAfter)+ fromProceedingString;
+                    String txPosEnd = (distanceToPreceedingExonEnd<distanceToProceedingExonEnd) ? String.valueOf(cdsLastBefore)+toPreceedingString : String.valueOf(cdsFirstAfter)+toProceedingString;
 
                     if(seqChange.size()==1){
                         txPos= txPosStart;
@@ -57,9 +70,9 @@ public class IntronChange extends TranscriptChange {
                         }
                     }
                 }else if(seqChange.isIns()){
-                    txPos = (distanceToPrecedingExonStart<distanceToProcedingExonStart) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPrecedingExonStart+1)+"_"+String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPrecedingExonStart+2) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProcedingExonStart)+"_"+String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProcedingExonStart+1);
+                    txPos = (distanceToPreceedingExonStart<distanceToProceedingExonStart) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPreceedingExonStart+1)+"_"+String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPreceedingExonStart+2) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProceedingExonStart)+"_"+String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProceedingExonStart+1);
                 }else{
-                    txPos = (distanceToPrecedingExonStart<distanceToProcedingExonStart) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPrecedingExonStart) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProcedingExonStart);
+                    txPos = (distanceToPreceedingExonStart<distanceToProceedingExonStart) ? String.valueOf(cdsLastBefore)+"+"+String.valueOf(distanceToPreceedingExonStart) : String.valueOf(cdsFirstAfter)+"-"+String.valueOf(distanceToProceedingExonStart);
                 }
 
 
